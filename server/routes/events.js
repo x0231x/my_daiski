257a070b4431052a794e188478a0cc69ab5ad439
x0231x <g534894123@gmail.com>
2025-05-12 18:24:51 +0800

    移除很多不需要的檔案 解決line-login vercel報錯問題-bui

--- server/routes/events.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.status(200).json({ status: 'success', message: 'Express(path: /api/events)' })
-})
-
-export default router
